d7457db73f9d48845d60277d7d05a049026d3d2a
Stephen Wu <>
2018-08-15 20:37:15 -0700

    Update mongo connection config

--- data/config.js
+++ data/config.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1', {
+  useMongoClient: true,
+});
 
 var db = mongoose.connection;
 
